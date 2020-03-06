3faaebb291ff223c95c55cf32e63441eefcd8bf2
Krishna-Koyyalamudi <60024842+Krishna-Koyyalamudi@users.noreply.github.com>
2020-03-06 15:54:21 -0600

    Added scripts/usdtoinr.js

---
+++ scripts/usdtoinr.js
@@ -0,0 +1,8 @@
+function convertinr() {
+    if (document.getElementById("c").value != " ") {
+        var f = document.getElementById("c").value * 1 * 72;
+        document.getElementById("d").value = Math.round(f);
+    } else {
+        document.getElementById("d").value = "please enter the usd in above textbox";
+    }
+}
