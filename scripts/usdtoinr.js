e2f1ead7acff9db2217b8d86a7bc2316506a6890
Krishna-Koyyalamudi <60024842+Krishna-Koyyalamudi@users.noreply.github.com>
2020-03-06 16:09:55 -0600

    remove parseint

--- scripts/usdtoinr.js
+++ scripts/usdtoinr.js
@@ -2,8 +2,8 @@ function convertinr() {
 
     if (document.getElementById("c").value != " ") {
         let ci = parseInt(document.getElementById("c"));
-        let d = ci * 1 * 72;
-        parseInt(document.getElementById("d")) = Math.round(d);
+        let f = ci * 1 * 72;
+        parseInt(document.getElementById("d")) = Math.round(f);
     } else {
         document.getElementById("d").value = "please enter the usd in above textbox";
     }
