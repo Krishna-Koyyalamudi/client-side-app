4a95c04aac164b7b0c26320b70d1662474ff585e
Krishna-Koyyalamudi <60024842+Krishna-Koyyalamudi@users.noreply.github.com>
2020-03-06 16:06:25 -0600

    d value

--- scripts/usdtoinr.js
+++ scripts/usdtoinr.js
@@ -2,7 +2,7 @@ function convertinr() {
 
     if (document.getElementById("c").value != " ") {
         let ci = parseInt(document.getElementById("c"));
-        let f = ci * 1 * 72;
+        let d = ci * 1 * 72;
         document.getElementById("d").value = Math.round(f);
     } else {
         document.getElementById("d").value = "please enter the usd in above textbox";
